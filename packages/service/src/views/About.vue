f737ff996392c55ce4a16a2178f27538c92c2326
ghpark <ghpark@daou.co.kr>
2021-12-29 17:46:14 +0900

    lerna

---
+++ packages/service/src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="about">
+		<h1>This is an about page</h1>
+		<b-button>버튼</b-button>
+	</div>
+</template>
+<script>
+import { core } from '@apps/core/lib/core.js';
+export default {
+	name: 'About',
+	created() {
+		console.log('about created');
+		core('zzzz');
+	},
+};
+</script>
---
+++ packages/service/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/logo.png" />
+		<HelloWorld msg="Welcome to Your Vue.js App" />
+		<Halo msg="core package halo~~~"></Halo>
+	</div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+import Halo from '@apps/core/components/Halo.vue';
+
+export default {
+	name: 'Home',
+	components: {
+		HelloWorld,
+		Halo,
+	},
+};
+</script>
